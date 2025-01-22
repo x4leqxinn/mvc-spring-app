aef9a4187c077f1f81f2f5b18f081b34035ac3c5
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-01-22 03:41:53 -0300

    add(view): Thymeleaf added

--- src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserController.java
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.x4leqxinn.springboot.webapp.springboot_webapp.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title", "Hello world =)");
+        model.addAttribute("name", "Jorge");
+        model.addAttribute("lastname", "Quintui");
         return "details";
     }
 }
