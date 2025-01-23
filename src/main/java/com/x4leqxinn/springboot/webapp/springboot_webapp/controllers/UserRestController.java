462a5c1b8c32d0ced9700d74ab37e8df22c930a7
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-01-23 02:38:20 -0300

    add(controller): @RequestMapping annotation added

--- src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserRestController.java
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserRestController.java
@@ -3,12 +3,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// Podemos usar @Controller - @ResponseBody
+
+// Más recomendado usar @RestController
+
 @RestController
+@RequestMapping("/api") // Asignamos una ruta base de primer nivel
 public class UserRestController {
 
-    @GetMapping("/details2") 
+    // Distintas formas de representar el mapping
+    @RequestMapping(path = "/details2",method = RequestMethod.GET) // Ruta secundaria del metodo
     public Map<String, Object> details(){
         Map<String, Object> response = new HashMap<>();
         response.put("title", "Hello world =)");
