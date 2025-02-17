99fd562b928032afdc49ae176e2b9f554ae561e2
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-02-17 03:19:02 -0300

    add(thymeleaf): List added

--- src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserController.java
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.x4leqxinn.springboot.webapp.springboot_webapp.controllers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.x4leqxinn.springboot.webapp.springboot_webapp.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
@@ -16,4 +21,13 @@ public class UserController {
         model.put("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+        model.addAttribute("users",users);
+        model.addAttribute("title","Listado usuarios");
+        return "list";
+    }
+    
 }
