1c41661bccf413ea06c4632a913bee82b8c5a09a
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-02-19 03:08:33 -0300

    add(api): RequestParam added

---
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/RequestParamController.java
@@ -0,0 +1,22 @@
+package com.x4leqxinn.springboot.webapp.springboot_webapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.x4leqxinn.springboot.webapp.springboot_webapp.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController 
+@RequestMapping("/api/params")
+public class RequestParamController {
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam(required = false, defaultValue = "-", name = "mensaje") String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
+}
---
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/models/dto/ParamDto.java
@@ -0,0 +1,13 @@
+package com.x4leqxinn.springboot.webapp.springboot_webapp.models.dto;
+
+public class ParamDto {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
