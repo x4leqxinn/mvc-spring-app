27725e43d9a9facdc762ecb1741112a74be292fb
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-02-21 06:37:10 -0300

    add(enpoint): Multiple variables added

--- src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.x4leqxinn.springboot.webapp.springboot_webapp.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +22,15 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+    
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
     }
     
 }
