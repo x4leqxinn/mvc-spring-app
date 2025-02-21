8b2caf038b0dbe357f968b1bc1fe62977742e15b
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-02-21 06:30:12 -0300

    add(endpoint): path variable added

---
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
@@ -0,0 +1,24 @@
+package com.x4leqxinn.springboot.webapp.springboot_webapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.x4leqxinn.springboot.webapp.springboot_webapp.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+    
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
+}
