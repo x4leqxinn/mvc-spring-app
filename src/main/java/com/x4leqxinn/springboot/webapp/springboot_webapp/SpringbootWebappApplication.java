a71e1299f545b393b083b173c974f122b1b6b153
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-02-22 03:43:15 -0300

    add(properties): Propertysources added

--- src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/SpringbootWebappApplication.java
+++ src/main/java/com/x4leqxinn/springboot/webapp/springboot_webapp/SpringbootWebappApplication.java
@@ -3,9 +3,12 @@ package com.x4leqxinn.springboot.webapp.springboot_webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+})
 public class SpringbootWebappApplication {
 
 	public static void main(String[] args) {
